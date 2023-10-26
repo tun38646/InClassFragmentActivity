e2fb1b886ecd8d7c231dc27ce3ef123a59eac61b
Timothy Bui <74019106+tun38646@users.noreply.github.com>
2023-10-26 15:25:41 -0400

    RecyclerView's adapter is now updated when ImageViewModel's LiveData object is updated.

--- app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
+++ app/src/main/java/edu/temple/inclassactivity/ImageDisplayFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -40,8 +41,12 @@ class ImageDisplayFragment : Fragment() {
         // The recycler view is the root element of the Fragment's layout
         // as such the view argument passed to onViewCreated() is the RecyclerView
         with (view as RecyclerView) {
-            if (::images.isInitialized)
-                adapter = CustomRecyclerAdapter(images)
+
+            ViewModelProvider(requireActivity())[ImagesViewModel::class.java]
+                .getImages().observe(viewLifecycleOwner){
+                    adapter = CustomRecyclerAdapter(it)
+                }
+
             layoutManager = GridLayoutManager(requireContext(), 2)
         }
     }
